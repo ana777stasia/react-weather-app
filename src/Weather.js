79151c395bede4c4bf5ccdb9292417b93235b036
ana777stasia <ppc1312@gmail.com>
2022-08-18 15:41:25 +0200

    added API

--- src/Weather.js
+++ src/Weather.js
@@ -2,61 +2,79 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Berlin",
-    temparature: 19,
-    description: "Clear Sky",
-    humidity: 50,
-    windSpeed: 10,
+export default function Weather(props) {
+  let weatherInfo = {
     currentHour: 12,
     currentMinute: 53,
     imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
   };
-  return (
-    <div className="Weather">
-      <form className="d-flex" role="search" id="search-form">
-        <input
-          className="form-control me-2 rounded-pill search-field"
-          type="search"
-          placeholder="Enter your city..."
-          aria-label="Search"
-        />
-        <button
-          className="btn btn-outline-secondary rounded-pill search-button"
-          type="submit"
-        >
-          Search
-        </button>
-      </form>
-      <div className="row weather-data">
-        <div className="col-12 current-weather-data">Current weather</div>
-        <div className="col-4 city-name">Berlin</div>
-        <div className="col-4 current-temperature">
-          {weatherData.temparature}
-          <span className="celsius">°C</span>
-        </div>
-        <div className="col-4 sky-data current-weather-data">
-          {weatherData.description}
-        </div>
-        <div className="col-4 current-weather-data">
-          {weatherData.currentHour}:{weatherData.currentMinute}
-          <div className="col-12 current-weather-data">12 August 2022</div>
-        </div>
-        <div className="col-4">
-          <img
-            src={weatherData.imgUrl}
-            alt="sun"
-            className="weather-icon main"
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function displayWeather(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+    });
+  }
+  function search() {
+    const apiKey = "e18f610bb5a857482b560fc01e23f479";
+    const units = "metric";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`;
+    axios.get(url).then(displayWeather);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form className="d-flex" role="search" id="search-form">
+          <input
+            className="form-control me-2 rounded-pill search-field"
+            type="search"
+            placeholder="Enter your city..."
+            aria-label="Search"
           />
-        </div>
-        <div className="col-4 current-weather-data">
-          Humidity: {weatherData.humidity}%
-          <div className="col-12 current-weather-data">
-            Wind speed: {weatherData.windSpeed}km/h
+          <button
+            className="btn btn-outline-secondary rounded-pill search-button"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+        <div className="row weather-data">
+          <div className="col-12 current-weather-data">Current weather</div>
+          <div className="col-4 city-name">{props.defaultCity}</div>
+          <div className="col-4 current-temperature">
+            {Math.round(weatherData.temperature)}
+            <span className="celsius">°C</span>
+          </div>
+          <div className="col-4 sky-data current-weather-data">
+            {weatherData.description}
+          </div>
+          <div className="col-4 current-weather-data">
+            {weatherInfo.currentHour}:{weatherInfo.currentMinute}
+            <div className="col-12 current-weather-data">12 August 2022</div>
+          </div>
+          <div className="col-4">
+            <img
+              src={weatherInfo.imgUrl}
+              alt="sun"
+              className="weather-icon main"
+            />
+          </div>
+          <div className="col-4 current-weather-data">
+            Humidity: {weatherData.humidity}%
+            <div className="col-12 current-weather-data">
+              Wind speed: {Math.round(weatherData.wind)}km/h
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }

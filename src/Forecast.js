2ced0f3772918409cb726d0b2f053e41d8e601c1
ana777stasia <ppc1312@gmail.com>
2022-08-23 15:28:12 +0200

    fixed a warning

--- src/Forecast.js
+++ src/Forecast.js
@@ -26,6 +26,8 @@ export default function Forecast(props) {
                 <WeatherForecastDay data={forecastData} />
               </div>
             );
+          } else {
+            return null
           }
         })}
       </div>
